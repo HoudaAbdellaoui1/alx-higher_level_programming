030b9eb0335bcdad97214c767c201a90cee52d28
HoudaAbdellaoui1 <78303915+HoudaAbdellaoui1@users.noreply.github.com>
2023-12-02 19:00:03 +0100

    Infinite add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -6,10 +6,10 @@ if __name__ == "__main__":
 
     counter = 0
     i = 1
+    
     if len(sys.argv) > 0:
         while i < len(sys.argv):
             if sys.argv[i] != '':
                 counter += int(sys.argv[i])
                 i+=1
-                
         print("{}".format(counter))
