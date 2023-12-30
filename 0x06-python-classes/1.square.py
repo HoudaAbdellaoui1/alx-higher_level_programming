e57bce57142dff3bd96e0180747082d4de6f0a08
HoudaAbdellaoui1 <78303915+HoudaAbdellaoui1@users.noreply.github.com>
2023-12-31 00:56:13 +0100

    Square with size

---
+++ 0x06-python-classes/1.square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size):
+        """
+        Initializes class attributes
+
+        Args:
+            size: size of square
+        """
+        self.__size = size
