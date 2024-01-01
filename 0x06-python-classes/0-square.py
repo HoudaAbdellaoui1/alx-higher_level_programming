2dd2874997c1ff8a25b3369703013e56e0b30da7
HoudaAbdellaoui1 <78303915+HoudaAbdellaoui1@users.noreply.github.com>
2024-01-01 19:58:18 +0100

    Define square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 class Square:
-    pass
+    """
+    This class defines a square.
+    """
+    def __init__(self):
+        """
+        Initializes a new instance of the Square class.
+
+        This method has no parameters as the square is initially empty.
+        """
+        pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 class Square:
-    def __init__(self, size) -> None:
-        """
-        Initializes class attributes
+    """
+    This class defines a square.
 
-        Args:
-            size: size of square
+    A square is a geometric shape with all four sides of equal length and
+    all interior angles right angles (90 degrees).
+    """
+    def __init__(self, size):
         """
+        Initializes a new instance of the Square class.
+
+        This method has no parameters as the square is initially empty.
+        """
+    pass
         self.__size = size
