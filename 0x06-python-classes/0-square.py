2921b579e693f1035d318a5717bed9e52d0cf329
HoudaAbdellaoui1 <78303915+HoudaAbdellaoui1@users.noreply.github.com>
2024-01-01 20:07:30 +0100

    Define square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
+"""Defines a square """
+
 class Square:
     """
     This class defines a square.
     """
-    def __init__(self):
-        """
-        Initializes a new instance of the Square class.
-
-        This method has no parameters as the square is initially empty.
-        """
-        pass
+    pass
