b45707167178539f66ce5f36d695001ff4adfab0
HoudaAbdellaoui1 <78303915+HoudaAbdellaoui1@users.noreply.github.com>
2024-01-07 00:35:28 +0100

    Apply pycodestyles

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -4,12 +4,12 @@
 
 class Rectangle:
     """Represent a rectangle.
-    
+
     Attributes:
         number_of_instances (int): The number of Rectangle instances.
         print_symbol : string representation
     """
-    
+
     number_of_instances = 0
     print_symbol = '#'
 
@@ -53,13 +53,13 @@ class Rectangle:
     def area(self):
         """Return the area of the Rectangle."""
         return (self.__width * self.__height)
-    
+
     def perimeter(self):
         """Return the perimeter of the Rectangle."""
         if self.__width == 0 or self.__height == 0:
             return (0)
         return ((self.__width * 2) + (self.__height * 2))
-    
+
     def __str__(self):
         """Return the printable representation of the Rectangle.
 
@@ -74,15 +74,14 @@ class Rectangle:
             if i != self.__height - 1:
                 rect.append("\n")
         return ("".join(rect))
-    
+
     def __repr__(self):
         """Return the string representation of the Rectangle."""
         rect = "Rectangle(" + str(self.__width)
         rect += ", " + str(self.__height) + ")"
         return (rect)
-    
+
     def __del__(self):
         """Print bye Rectangle on rectangle deletion"""
         type(self).number_of_instances -= 1
         print("Bye rectangle...")
-        
