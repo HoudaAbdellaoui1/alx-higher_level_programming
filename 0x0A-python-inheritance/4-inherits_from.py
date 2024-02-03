582ed469301088bc3861c490934a55c6c5a51336
HoudaAbdellaoui1 <78303915+HoudaAbdellaoui1@users.noreply.github.com>
2024-02-03 17:38:06 +0100

    Fix code styling

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -8,7 +8,4 @@ from the specific class or not.
 def inherits_from(obj, a_class):
     """Check if the object is an instance of a class that inherited (directly or indirectly)
     from the specified class."""
-    obj_class = type(obj)
-    if obj_class == a_class:
-        return False
-    return issubclass(obj_class, a_class)
+    return(issubclass(type(obj), a_class) and type(obj) != a_class)
