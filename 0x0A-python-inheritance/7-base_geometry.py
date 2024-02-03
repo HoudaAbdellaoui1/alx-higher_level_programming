491734402d6004a0902921461c2df3ab007444cd
HoudaAbdellaoui1 <78303915+HoudaAbdellaoui1@users.noreply.github.com>
2024-02-03 18:00:37 +0100

    Base geometry with area & integer_validator

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -3,15 +3,14 @@
 
 
 class BaseGeometry:
-    """A class with public attribute area"""
+    """A class with public attribute area and integer validator"""
     def area(self):
         """raises an Exception with the message 'area() is not implemented'."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Public instance method that validates the value:"""
+        """Public instance method that validates the value"""
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
-
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
