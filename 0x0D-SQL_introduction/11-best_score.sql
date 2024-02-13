97f5effc55a2eafd08dfba7718239d112a68c364
HoudaAbdellaoui1 <abdellaouihouda2@gmail.com>
2024-02-13 15:08:10 +0100

    fix codestyle

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,4 +1,2 @@
 -- display records with score greater than 10
-SELECT `score`, `name` from second_table
-WHERE `score` >= 10
-ORDER BY `score` DESC;
+SELECT `score`, `name` from second_table WHERE `score` >= 10 ORDER BY `score` DESC;
