b79ceb6d5cb636037055f07a7f715d17272f7e0e
HoudaAbdellaoui1 <abdellaouihouda2@gmail.com>
2024-02-14 10:54:12 +0100

    Join without a JOIN KEYWORD

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,9 +1,14 @@
 -- join tables
-SELECT id, name
-FROM cities
-WHERE state_id = (
-    SELECT id 
-    FROM states 
-    WHERE name = 'California'
-)
-ORDER BY id ASC;
+INSERT INTO cities (state_id, name) VALUES (1, 'San Jose')
+INSERT INTO cities (state_id, name) VALUES (2, 'Page')
+INSERT INTO cities (state_id, name) VALUES (3, 'Paris')
+INSERT INTO cities (state_id, name) VALUES (3, 'Houston')
+INSERT INTO cities (state_id, name) VALUES (3, 'Dallas')
+-- SELECT id, name
+-- FROM cities
+-- WHERE state_id = (
+--     SELECT id 
+--     FROM states 
+--     WHERE name = 'California'
+-- )
+-- ORDER BY id ASC;
