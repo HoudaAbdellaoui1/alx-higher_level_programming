1c89aab38c5314d9bb672651cf9f3346af377522
HoudaAbdellaoui1 <abdellaouihouda2@gmail.com>
2024-02-14 13:16:09 +0100

    Not mu genre

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,8 +1,6 @@
 -- list all shows from hbtn_0d_tvshows_rate by their rating
-SELECT tv_shows.title
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating_sum
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_genres.name <> 'Comedy' OR tv_genres.name IS NULL
+JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
 GROUP BY tv_shows.title
-ORDER BY tv_shows.title ASC;
+ORDER BY rating_sum DESC;
