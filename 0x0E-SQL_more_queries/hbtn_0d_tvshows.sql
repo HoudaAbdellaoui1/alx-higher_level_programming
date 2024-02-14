1e20c09edf4de915a9de26c43caf00d0025864ec
HoudaAbdellaoui1 <abdellaouihouda2@gmail.com>
2024-02-14 11:18:46 +0100

    List linked genres

--- 0x0E-SQL_more_queries/hbtn_0d_tvshows.sql
+++ 0x0E-SQL_more_queries/hbtn_0d_tvshows.sql
@@ -18,11 +18,12 @@
 --
 -- Table structure for table `tv_genres`
 --
+CREATE DATABASE IF NOT EXISTS hbtn_0d_tvshows;
 
-DROP TABLE IF EXISTS `tv_genres`;
+DROP TABLE IF EXISTS hbtn_0d_tvshows.tv_genres;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `tv_genres` (
+CREATE TABLE hbtn_0d_tvshows.tv_genres (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(256) NOT NULL,
   PRIMARY KEY (`id`)
@@ -33,20 +34,19 @@ CREATE TABLE `tv_genres` (
 -- Dumping data for table `tv_genres`
 --
 
-LOCK TABLES `tv_genres` WRITE;
+LOCK TABLES hbtn_0d_tvshows.tv_genres WRITE;
 /*!40000 ALTER TABLE `tv_genres` DISABLE KEYS */;
-INSERT INTO `tv_genres` VALUES (1,'Drama'),(2,'Mystery'),(3,'Adventure'),(4,'Fantasy'),(5,'Comedy'),(6,'Crime'),(7,'Suspense'),(8,'Thriller');
+INSERT INTO hbtn_0d_tvshows.tv_genres VALUES (1,'Drama'),(2,'Mystery'),(3,'Adventure'),(4,'Fantasy'),(5,'Comedy'),(6,'Crime'),(7,'Suspense'),(8,'Thriller');
 /*!40000 ALTER TABLE `tv_genres` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
 -- Table structure for table `tv_show_genres`
 --
-
 DROP TABLE IF EXISTS `tv_show_genres`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `tv_show_genres` (
+CREATE TABLE hbtn_0d_tvshows.tv_show_genres (
   `show_id` int(11) NOT NULL,
   `genre_id` int(11) NOT NULL,
   KEY `show_id` (`show_id`),
@@ -60,7 +60,7 @@ CREATE TABLE `tv_show_genres` (
 -- Dumping data for table `tv_show_genres`
 --
 
-LOCK TABLES `tv_show_genres` WRITE;
+LOCK TABLES hbtn_0d_tvshows.tv_show_genres WRITE;
 /*!40000 ALTER TABLE `tv_show_genres` DISABLE KEYS */;
 INSERT INTO `tv_show_genres` VALUES (1,1),(1,2),(2,3),(2,1),(2,4),(3,5),(4,5),(5,5),(6,6),(6,1),(6,7),(6,8),(8,6),(8,1),(8,2),(8,7),(8,8),(10,5),(10,1);
 /*!40000 ALTER TABLE `tv_show_genres` ENABLE KEYS */;
@@ -70,7 +70,7 @@ UNLOCK TABLES;
 -- Table structure for table `tv_shows`
 --
 
-DROP TABLE IF EXISTS `tv_shows`;
+DROP TABLE IF EXISTS hbtn_0d_tvshows.tv_shows;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tv_shows` (
@@ -84,9 +84,9 @@ CREATE TABLE `tv_shows` (
 -- Dumping data for table `tv_shows`
 --
 
-LOCK TABLES `tv_shows` WRITE;
+LOCK TABLES hbtn_0d_tvshows.tv_shows WRITE;
 /*!40000 ALTER TABLE `tv_shows` DISABLE KEYS */;
-INSERT INTO `tv_shows` VALUES (1,'House'),(2,'Game of Thrones'),(3,'The Big Bang Theory'),(4,'New Girl'),(5,'Silicon Valley'),(6,'Breaking Bad'),(7,'Better Call Saul'),(8,'Dexter'),(9,'Homeland'),(10,'The Last Man on Earth');
+INSERT INTO hbtn_0d_tvshows.tv_shows VALUES (1,'House'),(2,'Game of Thrones'),(3,'The Big Bang Theory'),(4,'New Girl'),(5,'Silicon Valley'),(6,'Breaking Bad'),(7,'Better Call Saul'),(8,'Dexter'),(9,'Homeland'),(10,'The Last Man on Earth');
 /*!40000 ALTER TABLE `tv_shows` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
