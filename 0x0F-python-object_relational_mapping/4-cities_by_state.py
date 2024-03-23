4b6fa89b4a99c328074a0ba75e49b790b085e1cc
HoudaAbdellaoui1 <78303915+HoudaAbdellaoui1@users.noreply.github.com>
2024-03-23 01:49:55 +0000

    fix codestyle

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -7,12 +7,13 @@ import MySQLdb
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         sys.exit(1)
-    
+
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
-    conn = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database, charset="utf8")
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                                passwd=password, db=database, charset="utf8")
     cur = conn.cursor()
     query = "SELECT * FROM states WHERE name =%s ORDER BY id ASC"
     cur.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC")
