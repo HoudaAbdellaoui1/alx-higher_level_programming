c3a85c896abeb1ffd580a0c0d5b4945db354fc34
HoudaAbdellaoui1 <78303915+HoudaAbdellaoui1@users.noreply.github.com>
2024-03-23 02:41:21 +0000

    moedl state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,7 +5,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, create_engine
 
 Base = declarative_base()
+
+
 class State(Base):
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False, unique= True)
-    name =  Column(String(128), nullable=False)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False,
+                unique=True)
+    name = Column(String(128), nullable=False)
