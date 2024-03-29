ddca49c61b2298a4e70e0f194f351424a124d9f1
Houda Abdellaoui <abdellaouihouda@gmail.com>
2024-03-29 00:27:32 +0000

    Advanced tasks

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send a POST request with specific data to trigger the desired response
-curl -s -X POST "0.0.0.0:5000/catch_me" -d "user_id=98" -H "Content-Type: application/x-www-form-urlencoded"
+curl -sL -X PUT "0.0.0.0:5000/catch_me" -d "user_id=98" -H "Content-Type: application/x-www-form-urlencoded"
