f0de1786aecbdbdefe5c15e381d3e1430766f9bb
Houda Abdellaoui <abdellaouihouda@gmail.com>
2024-03-30 22:43:20 +0000

    get 10 latest commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,8 +7,8 @@ if __name__ == '__main__':
     repository = sys.argv[1]
     owner = sys.argv[2]
 
-    url =  f'https://api.github.com/repos/{owner}/{repository}/commits'
-    params = {'per_page': 10, 'page' : 1 }
+    url = f'https://api.github.com/repos/{owner}/{repository}/commits'
+    params = {'per_page': 10, 'page': 1}
     response = requests.get(url, params=params)
     if response.status_code == 200:
         for c in response.json():
