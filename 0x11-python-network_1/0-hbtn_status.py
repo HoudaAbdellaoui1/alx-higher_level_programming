813784649c755e29d92aa85c21b265644b0b9224
Houda Abdellaoui <abdellaouihouda@gmail.com>
2024-03-30 04:16:11 +0000

    POST req using requests pkg + email param

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,8 +5,5 @@ if __name__ == '__main__':
     import urllib.request
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         the_page = response.read()
-    print("""Body response:
-        - type : {}
-        - content : {}
-        - utf8 content : {}"""
-        .format(type(the_page), the_page, the_page.decode('utf-8')))
+    print("""Body response:\n\t- type : {}\n\t- content : {}\n\t- utf8 content : {}"""
+          .format(type(the_page), the_page, the_page.decode('utf-8')))
