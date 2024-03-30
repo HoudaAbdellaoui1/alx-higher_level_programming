3e5a6b52db9eb8a516f2e000d0dcb841d8f1a0ae
Houda Abdellaoui <abdellaouihouda@gmail.com>
2024-03-30 05:24:59 +0000

    Fix USE WITH

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -5,13 +5,12 @@ Fetches a URL and displays its response body.
 Usage: ./3-error_code.py <URL>
 """
 import sys
-import urllib.request
+from urllib import request, error
 
 
 if __name__ == '__main__':
     try:
-        with urllib.request.Request(sys.argv[1]) as req:
-            resp = urllib.request.urlopen(req).read()
-            print(resp.decode('UTF-8'))
-    except urllib.error.HTTPError as e:
+        with request.urlopen(sys.argv[1]) as res:
+            print(res.read().decode('UTF-8'))
+    except error.HTTPError as e:
         print('Error code:', e.code)
