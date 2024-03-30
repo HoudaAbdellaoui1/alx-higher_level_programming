ccd679a976810e2dd0d76bda16935c2d6e2bd334
Houda Abdellaoui <abdellaouihouda@gmail.com>
2024-03-30 04:07:18 +0000

    Fetch using requests

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Dsplay header value using requests package"""
+
+import requests
+import sys
+if __name__ == '__main__':
+    r = requests.get(sys.argv[1])
+    print(r.headers['X-Request-Id'])
+
