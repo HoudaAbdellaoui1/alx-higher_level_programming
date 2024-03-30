1a04f9d2e939e34d99d92796f28d83803f769602
Houda Abdellaoui <abdellaouihouda@gmail.com>
2024-03-30 04:10:14 +0000

    POST req using requests pkg + email param

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""POST email param using requests package"""
+
+import requests
+import sys
+if __name__ == '__main__':
+    r = requests.post(sys.argv[1], data={'email': sys.argv[2]})
