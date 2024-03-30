f4c3e7c8b4698dd605afc47229eed88823e530d8
Houda Abdellaoui <abdellaouihouda@gmail.com>
2024-03-30 05:09:53 +0000

    Fix USE WITH

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-"""Display response body """
+"""
+Fetches a URL and displays its response body.
 
+Usage: fetch_url.py <URL>
+"""
 import sys
 import urllib.request
 import urllib.parse
 if __name__ == '__main__':
-    with urllib.request.Request(sys.argv[1]) as req:
-        try:
+    try:
+        with urllib.request.Request(sys.argv[1]) as req:
             resp = urllib.request.urlopen(req).read()
             print(resp.decode('utf-8'))
-        except urllib.error.HTTPError as e:
-            print('Error code: ', e.code)
+    except urllib.error.HTTPError as e:
+        print('Error code:', e.code)
