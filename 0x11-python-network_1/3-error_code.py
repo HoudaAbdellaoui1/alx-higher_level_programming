d2c78f65fa9993bdc0589203aa24b48a4b54d2a8
Houda Abdellaoui <abdellaouihouda@gmail.com>
2024-03-30 03:53:08 +0000

    Manage HTTP error exceptions

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -7,7 +7,7 @@ import urllib.parse
 if __name__ == '__main__':
     req = urllib.request.Request(sys.argv[1])
     try:
-        resp = urllib.request.urlopen(req)
+        resp = urllib.request.urlopen(req).read()
         print(resp.decode('utf-8'))
     except urllib.error.HTTPError as e:
         print('Error code: ', e.code)
