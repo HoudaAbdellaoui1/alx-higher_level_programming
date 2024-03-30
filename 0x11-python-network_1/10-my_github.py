9f6a0feccc99f3bc00d6ed1b3c5f7e4e619a17c8
Houda Abdellaoui <abdellaouihouda@gmail.com>
2024-03-30 06:30:33 +0000

    search API

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,9 +5,7 @@ import requests
 import sys
 if __name__ == '__main__':
     username = sys.argv[1]
-    # username = 'HoudaAbdellaoui1'
     pwd = sys.argv[2]
-    # pwd = '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'
     url = '  https://api.github.com/users'
     payload = {'USERNAME': username}
     headers = {'Authorization' : ('Bearer {}'.format(pwd))}
