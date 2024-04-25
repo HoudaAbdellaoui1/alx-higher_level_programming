d1a8f5a14657f085620031fc53d85fbee5c8ac0b
Houda Abdellaoui <abdellaouihouda@gmail.com>
2024-04-25 09:32:17 +0100

    debug get movies

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const request = require('request');
+const movieId = process.argv[2];
+const getMovieUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`
+// Get the movie 
+request(getMovieUrl, (err, res, body) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  const characters = JSON.parse(body).characters;
+  for (const characterUrl of characters) {
+    request(characterUrl, (err, res, body) => {
+      if(err) {console.error(err); return;}
+      const characterName = JSON.parse(body).name
+          console.log(characterName);
+    })
+  }
+});
