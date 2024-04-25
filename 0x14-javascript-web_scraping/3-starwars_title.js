8acc59c94848ee786cd2a8047d9946d6f4c8e495
Houda Abdellaoui <abdellaouihouda@gmail.com>
2024-04-25 01:43:58 +0100

    Get request with params

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,11 +3,11 @@ const request = require('request');
 const movieId = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 request(url, (err, res, body) => {
-    if(err) {
-        console.error(err);
-        return;
-    }
+  if (err) {
+    console.error(err);
+    return;
+  }
 
-    const movie = JSON.parse(body);
-    console.log(movie.title);
+  const movie = JSON.parse(body);
+  console.log(movie.title);
 });
