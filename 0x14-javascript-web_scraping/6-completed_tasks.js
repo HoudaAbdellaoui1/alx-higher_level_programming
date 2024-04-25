4d73ed2f872fa413b9162f506035669faefa78c5
Houda Abdellaoui <abdellaouihouda@gmail.com>
2024-04-25 02:22:41 +0100

    Get request with params

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+const request = require('request');
+
+const apiUrl = process.argv[2];
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+  if (response.statusCode !== 200) {
+    console.error('Status:', response.statusCode);
+    return;
+  }
+
+  const todos = JSON.parse(body);
+
+  const completedTasksByUser = todos.reduce((acc, todo) => {
+    if (todo.completed) {
+      acc[todo.userId] = (acc[todo.userId] || 0) + 1;
+    }
+    return acc;
+  }, {});
+
+  Object.keys(completedTasksByUser).forEach(userId => {
+    console.log(`'${userId}': ${completedTasksByUser[userId]} tasks.`);
+  });
+});
