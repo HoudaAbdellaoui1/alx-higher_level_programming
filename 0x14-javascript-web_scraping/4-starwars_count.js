afb34e766efba91aa73cef0efdb7dcd455576601
Houda Abdellaoui <abdellaouihouda@gmail.com>
2024-04-25 02:05:32 +0100

    Get request with params

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -9,7 +9,8 @@ request(url, (err, res, body) => {
   }
   let counter = 0;
   for (let i = 0; i < body.results.length; i++) {
-    if (data.results[i].characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) { counter += 1; }
+    if (body.results[i].characters.includes('https://swapi-api.alx-tools.com/api/people/18/'))
+        counter += 1;
   }
   console.log(counter);
 });
