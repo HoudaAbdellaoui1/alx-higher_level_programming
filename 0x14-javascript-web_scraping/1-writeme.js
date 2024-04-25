12e754dfdaf073cbd848eea7dfe2ece462165652
Houda Abdellaoui <abdellaouihouda@gmail.com>
2024-04-25 01:22:23 +0100

    AppendFile

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const stringToWrite = process.argv[3];
 
-fs.open(filePath, stringToWrite, function (err) {
+fs.appendFile(filePath, stringToWrite, function (err) {
   if (err) throw err;
   console.log('Saved!');
 });
