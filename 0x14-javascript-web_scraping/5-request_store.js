6c56a2bda6298dcc02fce13d6b6306b472a3e968
Houda Abdellaoui <abdellaouihouda@gmail.com>
2024-04-25 02:18:07 +0100

    Get request with params

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,19 @@
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+  if (response.statusCode !== 200) {
+    console.error('Status:', response.statusCode);
+    return;
+  }
+
+  fs.writeFileSync(filePath, body, 'utf-8');
+});
